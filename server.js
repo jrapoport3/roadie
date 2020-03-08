5d8e9f35b7a9a0e2846a4fb58f8d97d954c405c7
Julia Rapoport <jrapoport@Julias-MacBook-Pro.local>
2020-03-08 19:23:46 -0400

    attempt 3 at getting heroku to work

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const path = require('path');
 const bodyParser = require("body-parser");
 
 const app = express();
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 const port = process.env.PORT || 3001;
 
